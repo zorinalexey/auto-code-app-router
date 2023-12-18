d81435e760713847e99b95a2b78850077be5922b
Зорин Алексей <zorinalexey59292@gmail.com>
2023-12-18 23:46:00 +0400

    debug router

--- src/Utils/ResponseXml.php
+++ src/Utils/ResponseXml.php
@@ -18,7 +18,7 @@ final class ResponseXml
 
     private function createSchema(mixed $data): string
     {
-        $rootElementName = 'response';
+        $rootElementName = config('app', 'xml_response_root_element_name', 'response');
         $attr['type'] = gettype($data);
 
         if ($attr['type'] === 'object') {
@@ -31,7 +31,7 @@ final class ResponseXml
             $xml->startElement($rootElementName, null, $attr);
 
             foreach ($data as $key => $value) {
-                $this->addElement($key, $value, $xml);
+                $this->createStructure($key, $value, $xml);
             }
         } else {
             $xml->startElement($rootElementName, $data, $attr);
@@ -43,7 +43,7 @@ final class ResponseXml
     }
 
 
-    private function addElement(int|string $key, mixed $value, XmlService $xml): void
+    private function createStructure(int|string $key, mixed $value, XmlService $xml): void
     {
         $attr['type'] = gettype($value);
 
@@ -51,23 +51,27 @@ final class ResponseXml
             $key = "_{$key}";
         }
 
+        if(is_file($value) && config('app', 'response_file_auto_encode', true)){
+            $value = base64_encode(file_get_contents($value));
+        }
+
         if (is_string($value)) {
-            $this->setTextXmlBlock($value, $xml, $key, $attr);
+            $this->setTextBlock($value, $xml, $key, $attr);
         } elseif (is_bool($value) || is_numeric($value)) {
             $xml->addElement($key, (string)$value, $attr);
         } elseif ($value === null) {
             $value = 'NULL';
             $xml->addElement($key, $value, $attr);
         } elseif ((is_array($value) || is_object($value)) && !($value instanceof Closure)) {
-            $this->setSuperXmlBlock($value, $xml, $key, $attr);
+            $this->setSuperBlock($value, $xml, $key, $attr);
         } elseif (($value instanceof Closure) || is_callable($value)) {
-            $this->setSerializableXmlBlock($value, $xml, $key, $attr);
+            $this->setSerializableBlock($value, $xml, $key, $attr);
         } else {
             $xml->addElement($key, null, [...$attr, 'error' => 'data type not supported']);
         }
     }
 
-    private function setTextXmlBlock(string $value, XmlService $xml, string|int $key, array $attr): void
+    private function setTextBlock(string $value, XmlService $xml, string|int $key, array $attr): void
     {
         if (strtotime($value)) {
             $format = 'Y-m-d H:i:s';
@@ -80,7 +84,7 @@ final class ResponseXml
         $xml->addElement($key, $value, $attr, createIfTextNull: true);
     }
 
-    private function setSuperXmlBlock(mixed $value, XmlService $xml, int|string $key, array $attr): void
+    private function setSuperBlock(mixed $value, XmlService $xml, int|string $key, array $attr): void
     {
         if ($attr['type'] === 'object') {
             $attr['class'] = $value::class;
@@ -89,13 +93,13 @@ final class ResponseXml
         $xml->startElement($key, null, $attr);
 
         foreach ($value as $k => $val) {
-            $this->addElement($k, $val, $xml);
+            $this->createStructure($k, $val, $xml);
         }
 
         $xml->closeElement();
     }
 
-    private function setSerializableXmlBlock(callable|Closure $value, XmlService $xml, int|string $key, array $attr): void
+    private function setSerializableBlock(callable|Closure $value, XmlService $xml, int|string $key, array $attr): void
     {
         $attr['class'] = $value::class;
         $attr['serialized_by'] = SerializableClosure::class;
