291d66771d3d1a8cd6ab6632e764144b9d579ba3
Зорин Алексей <zorinalexey59292@gmail.com>
2023-12-18 12:28:09 +0400

    start debug router

---
+++ src/Common/ResponseValidated.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AutoCode\AppRouter\Common;
+
+use AutoCode\AppRouter\Abstractions\AbstractRoute;
+use Closure;
+
+final class ResponseValidated
+{
+
+    private readonly AbstractRoute $route;
+
+    public function __construct(AbstractRoute $route)
+    {
+        $this->route = $route;
+
+        $this->response();
+    }
+
+    private function response()
+    {
+        $action = $this->route->getAction();
+
+        if(( $action instanceof Closure) || is_callable($action)){
+            $action();
+        }
+    }
+}
