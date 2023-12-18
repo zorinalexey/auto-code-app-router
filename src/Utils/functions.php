33d244cc4fe43a441784d974c9348bde15966478
Зорин Алексей <zorinalexey59292@gmail.com>
2023-12-18 13:28:18 +0400

    debug router

--- src/Utils/functions.php
+++ src/Utils/functions.php
@@ -8,7 +8,7 @@ if (!function_exists('getallheaders'))
 
         foreach ($_SERVER as $name => $value)
         {
-            if (str_starts_with(mb_strtoupper($name), 'HTTP_'))
+            if (preg_match('~HTTP_~ui', $name))
             {
                 $key = str_replace('HTTP_', '', $name);
                 $headers[$key] = $value;
